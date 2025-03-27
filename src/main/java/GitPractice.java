724054a541b12280b5f2d21ac1fc271bca662205
VijayKode <vijaymahendhra.kode@gmail.com>
2025-03-27 10:56:51 +0530

    merge check

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -1,15 +1,3 @@
 public class GitPractice {
-    /*
-        this is old1 line
-     */
-
-    // vipul code is there
-
-
-    // main code
-
-    // child code1
-
-
-    //new branch new
+    // merge check
 }
